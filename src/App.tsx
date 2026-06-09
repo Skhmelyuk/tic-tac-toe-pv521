d71739abcf5b7d98ec8940b12185a9b9071747b9
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-06-09 19:44:55 +0300

    skhmelyuk change project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import { TitleGame } from "./components/TitleGame";
 
 function App() {
+  const cells = [null, "X", null, null, null, null, "O", "O", null];
+
   return (
     <div className="game" id="game">
       <TitleGame />
       <div className="turn">Хід гравця X</div>
       <div className="board">
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
+        {cells.map((cell, index) => (
+          <div key={index} className="cell">
+            {cell}
+          </div>
+        ))}
       </div>
       <button className="reset">Скинути гру</button>
     </div>
