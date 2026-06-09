275018de3ea502175c72403100db180278cade03
арсений бобин <bobinarseniy17022010@gmail.com>
2026-06-09 18:56:41 +0300

    BobiArs: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1>Tic-Tac-Toe</h1>;
+  return (
+    <div class="game" id="game">
+      <h1 class="title">Гра христики-нулики</h1>
+      <div class="turn">Хід гравця X</div>
+      <div class="board">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+      <button class="reset">Скинути гру</button>
+    </div>
+  );
 }
 
 export default App;
