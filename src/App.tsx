719f7443caf7ef51092e5aed84e4a25ffbf6f1dd
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-06-09 21:12:04 +0300

    change APP

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,13 @@ function App() {
   return (
     <div className="game" id="game">
       <TitleGame title="Гра хрести нулики" age={20} />
-      <TitleGame title="Гра хрести нулики 1" age={40} />
-      <TitleGame title="Гра хрести нулики 2" age={60} />
-      <TitleGame title="Гра хрести нулики 3" age={80} />
-      <div className="turn">Хід гравця X</div>
+      <Status player={currentPlayer} />
       <div className="board">
         {cells.map((cell, index) => (
           <Cell value={cell} key={index} />
         ))}
       </div>
-      <button className="reset">Скинути гру</button>
+      <button className="reset">Скинути гру !!!!!</button>
     </div>
   );
 }
