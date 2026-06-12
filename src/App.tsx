f0de25a9d60c8e700a553f9eef6a201c4b366435
MusDev13 <sonics0977703996@gmail.com>
2026-06-12 20:25:04 +0300

    new types and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import { Cell } from "./components/Cell";
-import { Counter } from "./components/Counter";
 import { Status } from "./components/Status";
 import { TitleGame } from "./components/TitleGame";
-import type { BoardState, Player } from "./types";
+import type { BoardState, Player, WinResult } from "./types";
+
+const WINNING_COMBINATIONS: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], // Рядки (горизонтальні лінії)
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8], // Стовпці (вертикальні лінії)
+  [0, 4, 8],
+  [2, 4, 6], // Діагоналі
+]
+
+function checkWinner(currentBoard: BoardState): WinResult | null {
+  for (const combination of WINNING_COMBINATIONS) {
+    const [a, b, c] = combination;
+    if (
+      currentBoard[a] &&
+      currentBoard[a] === currentBoard[b] &&
+      currentBoard[a] === currentBoard[c]
+    ) {
+      return {
+        winner: currentBoard[a] as Player,
+        combination,
+      };
+    }
+  }
+  return null;
+}
 
 function App() {
-  const cells: BoardState = [null, "X", null, null, null, null, "O", "O", null];
-  const currentPlayer: Player = "X";
+  const [cells, setCells] = useState<BoardState>([null, "X", null, null, null, null, "O", "O", null]);
+  const [currentPlayer, setCurrentPlayer] = useState<Player>("X");
+  // const cells: BoardState = [null, "X", null, null, null, null, "O", "O", null];
+  // const currentPlayer: Player = "X";
 
   return (
     <div className="game" id="game">
-      <Counter />
       <TitleGame title="Гра хрести нулики" age={20} />
       <Status player={currentPlayer} />
       <div className="board">
