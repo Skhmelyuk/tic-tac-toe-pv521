acc6a1d4e0dd209303b91fa907d8ab607ad0a3cf
MusDev13 <sonics0977703996@gmail.com>
2026-06-14 20:24:04 +0300

    custom useGame hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,64 +3,20 @@ import { Cell } from "./components/Cell";
 import { Status } from "./components/Status";
 import { TitleGame } from "./components/TitleGame";
 import type { BoardState, Player, WinResult } from "./types";
+import useGame from "./hooks/useGame";
 
-const WINNING_COMBINATIONS: number[][] = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8], // Рядки (горизонтальні лінії)
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8], // Стовпці (вертикальні лінії)
-  [0, 4, 8],
-  [2, 4, 6], // Діагоналі
-];
 
-function checkWinner(currentBoard: BoardState): WinResult | null {
-  for (const combination of WINNING_COMBINATIONS) {
-    const [a, b, c] = combination;
-    if (
-      currentBoard[a] &&
-      currentBoard[a] === currentBoard[b] &&
-      currentBoard[a] === currentBoard[c]
-    ) {
-      return {
-        winner: currentBoard[a] as Player,
-        combination,
-      };
-    }
-  }
-  return null;
-}
 
 function App() {
-  const [cells, setCells] = useState<BoardState>([
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-  ]);
-  const [currentPlayer, setCurrentPlayer] = useState<Player>("X");
-
-  const winnerResult = checkWinner(cells);
-  const winner = winnerResult ? winnerResult.winner : null;
-  const winnerCombination = winnerResult ? winnerResult.combination : [];
-  const isDraw = !winner && cells.every((cell) => cell != null);
-
-  const handleSendClick = (index: number): void => {
-    if (cells[index] || winner || isDraw) {
-      return;
-    }
-
-    const newCells = [...cells];
-    newCells[index] = currentPlayer;
-    setCells(newCells);
-    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
-  };
+  const {
+    cells,
+    currentPlayer,
+    winner,
+    winnerCombination,
+    isDraw,
+    handleSendClick,
+    handleReset
+  } = useGame();
 
   return (
     <div className="game" id="game">
@@ -75,7 +31,7 @@ function App() {
           />
         ))}
       </div>
-      <button className="reset">Скинути гру !!!!!</button>
+      <button className="reset" onClick={handleReset}>Скинути гру</button>
     </div>
   );
 }
