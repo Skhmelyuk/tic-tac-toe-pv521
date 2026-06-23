24b29d8e6d5bd4e5c164776ac0263a0875a26d7c
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-06-23 19:56:43 +0300

    createContext

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,4 +1,6 @@
 import type { CellValue, Player } from "../types";
+import { useContext } from "react";
+import { RegisterContext } from "../main";
 
 interface StatusProps {
   player: Player;
@@ -7,12 +9,15 @@ interface StatusProps {
 }
 
 export function Status({ player, winner, isDraw }: StatusProps) {
+  const { user } = useContext(RegisterContext);
+
   if (winner) {
     return (
       <div className="turn">
         Гравець{" "}
         <span className={winner === "X" ? "x-mark" : "o-mark"}>{winner}</span>{" "}
         переміг!
+        <span className="">{user}</span>
       </div>
     );
   }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,27 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+import { createContext } from "react";
+
+interface RegisterUser {
+  user: string;
+  email: string;
+}
+
+export const RegisterContext = createContext<RegisterUser | undefined>(
+  undefined,
+);
+
+export const RegisterContext2 = createContext<RegisterUser | undefined>(
+  undefined,
+);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RegisterContext.Provider
+      value={{ user: "Hmelyuk", email: "skhmelyuk1985@gmail.com" }}
+    >
+      <App />
+    </RegisterContext.Provider>
   </StrictMode>,
 );
